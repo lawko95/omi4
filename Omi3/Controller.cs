1e4c47cab90efccc8143f646846eef2a99b0e288
Adriaan Kisjes <adriaan@Adriaans-MacBook-Pro.local>
2018-03-06 12:47:25 +0100

    Connection en Heuristic class toe gevoegt

--- Omi3/Controller.cs
+++ Omi3/Controller.cs
@@ -4,7 +4,7 @@ namespace Omi3
 	public class Controller
 	{
 		public BodyObject[] bodies;
-		private TwoBodies[] connections;
+        private Connection[] connections;
 		private double timeStep;
 
 		public Controller(BodyObject[] bs, double dt)
@@ -23,14 +23,15 @@ namespace Omi3
 
 		private void makeConnections()
 		{
-			connections = new TwoBodies[(bodies.Length * (bodies.Length - 1) / 2)];
+            connections = new Connection[(bodies.Length * (bodies.Length - 1) / 2)];
 			var tel = 0;
 			for (int i = 0; i < bodies.Length; i++)
 			{
 				for (int j = i + 1; j < bodies.Length; j++)
 				{
 					var tb = new TwoBodies(bodies[i], bodies[j]);
-					connections[tel] = tb;
+                    var c = new Connection(new Heuristic(), tb);
+					connections[tel] = c;
 					tel++;
 				}
 			}
@@ -38,8 +39,9 @@ namespace Omi3
 
 		private void calcNewForce()
 		{
-			foreach (TwoBodies tb in connections)
+            foreach (Connection c in connections)
 			{
+                var tb = c.TB;
 				var r = Math.Pow(tb.A.Location.Dist(tb.B.Location), 2.0);
 				var g = 6.674 * Math.Pow(10.0, -11.0);
 				var f = (tb.A.Mass * tb.B.Mass * g) / r;
---
+++ Omi3/HeuristicHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Omi3
+{
+	public class Connection : IComparable<Connection>
+	{
+		public double V;
+		public TwoBodies TB;
+		public Heuristic H;
+		public Connection(Heuristic h, TwoBodies tb)
+		{
+			V = 0;
+			TB = tb;
+			H = h;
+		}
+
+		public void Update(double f)
+		{
+			V = H.Calc(TB, f);
+		}
+
+		public int CompareTo(Connection obj)
+		{
+			if (this.V > obj.V) return -1;
+			return 1;
+		}
+	}
+
+    public class Heuristic{
+        public Heuristic(){
+            
+        }
+
+        public virtual double Calc(TwoBodies tb, double f){
+            return 0.0;
+        }
+    }
+
+    public class FDiv : Heuristic{
+        private double prefF;
+        public FDiv() : base(){
+            prefF = -1.0;
+        }
+
+        public override double Calc(TwoBodies tb, double f){
+            if (prefF < 0){
+                prefF = f;
+                return 0.0;
+            }
+            else{
+                double res = Math.Abs(prefF - f);
+                prefF = f;
+                return res;
+            }
+        }
+    }
+}
