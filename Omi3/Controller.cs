2e0004e6345b97a99e78791da3ecf32fcdd7a6bf
Adriaan Kisjes <adriaan@Adriaans-MacBook-Pro.local>
2018-03-14 16:06:43 +0100

    heuristic geimplementeerd(ish)

--- Omi3/Controller.cs
+++ Omi3/Controller.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Omi3
 {
 	public class Controller
@@ -6,17 +7,45 @@ namespace Omi3
 		public BodyObject[] bodies;
         private Connection[] connections;
 		private double timeStep;
+        private int uper;
+        private int prion;
+        private int upercount;
+        private bool heur;
 
-		public Controller(BodyObject[] bs, double dt)
+		public Controller(BodyObject[] bs, double dt, bool _heur, int _uper = 0, int _prion = 0)
 		{
 			bodies = bs;
 			timeStep = dt;
 			makeConnections();
+            uper = _uper;
+            prion = _prion;
+            upercount = 0;
+            heur = _heur;
 		}
 
 		public BodyObject[] DoStep()
 		{
-			calcNewForce();
+            if (heur){
+                if (upercount <= 2)
+                {
+                    calcNewForce(connections.Length, true);
+                }
+                else
+                {
+                    calcNewForce(prion, false);
+                }
+                if(upercount == 1){
+                    Array.Sort(connections);
+                }
+                upercount += 1;
+                if (upercount > uper)
+                {
+                    upercount = 0;
+                }
+            }
+            else{
+                calcNewForce(connections.Length, false);
+            }
 			doStep();
             return bodies;
         }
@@ -30,21 +59,25 @@ namespace Omi3
 				for (int j = i + 1; j < bodies.Length; j++)
 				{
 					var tb = new TwoBodies(bodies[i], bodies[j]);
-                    var c = new Connection(new Heuristic(), tb);
+                    var c = new Connection(new FDiv(), tb);
 					connections[tel] = c;
 					tel++;
 				}
 			}
 		}
 
-		private void calcNewForce()
+        private void calcNewForce(int count, bool update)
 		{
-            foreach (Connection c in connections)
+            for (int i = 0; i < count; i++)
 			{
+                var c = connections[i];
                 var tb = c.TB;
 				var r = Math.Pow(tb.A.Location.Dist(tb.B.Location), 2.0);
 				var g = 6.674 * Math.Pow(10.0, -11.0);
 				var f = (tb.A.Mass * tb.B.Mass * g) / r;
+                if (update){
+                    c.Update(f);
+                }
 				var bta = tb.A.Location.Dir(tb.B.Location);
 				var atb = tb.B.Location.Dir(tb.A.Location);
 				bta = bta.Mult(f);
--- Omi3/Program.cs
+++ Omi3/Program.cs
@@ -16,10 +16,11 @@ namespace Omi3
         {
             Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			var ls = new BodyObject[2];
+			var ls = new BodyObject[3];
 			ls[0] = new BodyObject(new Vector(1, 0), 3000000000, new Vector(0, -0.01), "a");
 			ls[1] = new BodyObject(new Vector(15, 0), 3000000000, new Vector(0, 0.01), "b");
-			var c = new Controller(ls, 1);
+			ls[2] = new BodyObject(new Vector(7, 0), 3000000000, new Vector(0, 0.03), "c");
+            var c = new Controller(ls, 1, true, ls.Length, ls.Length);
 			var f = new Form1(c);
 			Application.Run(f);
         }
