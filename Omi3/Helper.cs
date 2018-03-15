2b368faac5c59e5e57bc4763859f4d030b85a3d1
Adriaan Kisjes <adriaan@Adriaans-MacBook-Pro.local>
2018-03-15 11:35:06 +0100

    Bigger pixels

--- Omi3/Helper.cs
+++ Omi3/Helper.cs
@@ -3,12 +3,25 @@ namespace Omi3
 {
     public static class Helper
     {
-        public static double CalcDist(BodyObject[] a, BodyObject[] b){
+        public static double CalcDistAbs(BodyObject[] a, BodyObject[] b){
             double res = 0.0;
             for (int i = 0; i < a.Length && i < b.Length; i++){
                 res += Math.Pow(a[i].Location.Dist(b[i].Location), 2);
             }
             return res;
         }
+
     }
 }
+
+
+/*
+public static double CalcDistRel(BodydObject[] a, BodyObject[] b){
+	double res = 0.0;
+	for (int i = 0; i < a.Length && i < b.Length; i++){
+		for (int j = 0; j < a.Length &&j < b.Length; j++){
+			//res += Math.Abs()
+		}
+	}
+	return res;
+}*/
